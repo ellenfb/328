be31233c96e7153d0940931953450df7e271b30e
ellenfb <44880614+ellenfb@users.noreply.github.com>
2019-03-23 15:00:34 -0700

    Update Quick_select.java

--- Quick_select.java
+++ Quick_select.java
@@ -7,6 +7,9 @@
 /** Uses user input */
 import java.util.Scanner;
 
+/** Array functions */
+import java.util.Arrays;
+
 /** Generates random numbers */
 import java.util.Random;
 
@@ -19,24 +22,33 @@ public class Quick_select {
 	 * @param up Upper bound of algorithm
 	 * @return Partition
 	 */
-	public static int findPartition(int [] arr, int lo, int up) {
+	public static int findPartition(int arr[], int lo, int up) {
 		
-		int piv = arr[up], 
-		prt = lo;
+		/** Pivot is set to the beginning of the array */
+		int piv = arr[lo];
+		int prt = up;
 		int swap;
-		for (int i = lo; i <= up - 1; i++) { 
-			
-			if (piv >= arr[i]) { 
-				swap = arr[prt]; 
-				arr[prt] = arr[i]; 
-				arr[i] = swap; 
-				prt++; 
-            } 
-        } 
 		
-		swap = arr[prt]; 
-		arr[prt] = arr[up]; 
-		arr[up] = swap; 
+		/** Iterates down through array */
+		for (int i = up; i > lo; i--) {
+			
+			/**
+			 * If pivot is <= array element at i, swapped with element at position prt
+			 * prt is then decremented so the position before it is the location of the next swap if any
+			 */
+			if (piv <= arr[i]) {
+				
+				swap = arr[i];
+				arr[i] = arr[prt];
+				arr[prt] = swap;
+				prt--;
+			}
+		}
+		
+		/** prt and beginning of array are swapped, prt returned */
+		swap = arr[prt];
+		arr[prt] = arr[lo];
+		arr[lo] = swap;
 		return prt;
 	}
 	
@@ -49,9 +61,16 @@ public class Quick_select {
 	 */
 	public static void quickSelect(int [] arr, int lo, int up, int k) {
 		 
-		int prt = findPartition(arr, lo, up); 
+		/** Find partition value for first run through */
+		int prt = findPartition(arr, lo, up);
   
+		/** 
+		 * while loop because function does not use recursion 
+		 * k - 1 because decrementing by one finds position in array terms (arrays start at 0, not 1)
+		 * prt - lo because partition - lower bound equaling k - 1 signals the element is found on either array segment
+		 */
 		while(prt - lo != k - 1) {
+			
 			if (prt - lo > k - 1) {  
 				prt = findPartition(arr, lo, prt - 1);
 			}
@@ -64,29 +83,38 @@ public class Quick_select {
 	}
 	
 	/**
-	 * Finds and prints the three largest elements
+	 * Finds and prints the m largest elements
 	 * @param arr Array being searched
 	 * @param lo Lower bound
-	 * @param size Size of araay
+	 * @param size Size of array
+	 * @param m Largest numbers
 	 */
-	public static void quickSelectMax(int [] arr, int lo, int size) {
-		 
-		int up = size;
-		int prt = -1;
-		System.out.print("Max three = ");
+	public static void quickSelectMax(int [] arr, int lo, int size, int m) {
 		
-		for(int k = size - 1; k <= size+1; k++){
+		/** Find partition value for first run through */
+		int prt = findPartition(arr, lo, size);
+		
+		/** */
+		int k = size - m + 2;
+  
+		/** 
+		 * while loop because function does not use recursion 
+		 * k - 1 because decrementing by one finds position in array terms (arrays start at 0, not 1)
+		 * prt - lo because partition - lower bound equaling k - 1 signals the element is found
+		 */
+		while(prt - lo != k - 1) {
 			
-			prt = findPartition(arr, lo, up); 
-			while(prt - lo != k - 1) {
-				if (prt - lo > k - 1) {  
-					prt = findPartition(arr, lo, prt - 1);
-				}
-				else {
-					prt = findPartition(arr, prt + 1, up);
-				}
+			if (prt - lo > k - 1) {  
+				prt = findPartition(arr, lo, prt - 1);
 			}
-			System.out.print(arr[prt] + " ");
+			else {
+				prt = findPartition(arr, prt + 1, size);
+			}
+		}
+		
+		System.out.print("Max numbers = ");
+		for(int i = 0; i < m; i++) {
+			System.out.print(arr[prt + i] + " ");
 		}
 		
 	}
@@ -104,8 +132,8 @@ public class Quick_select {
 		int [] array = new int [size];
 		for(int i = 0; i < size; i++) {
 			array[i] = ran.nextInt(200)-100;
-			System.out.println("a[" + i + "] = " + array[i]);
 		}
+		System.out.println(Arrays.toString(array));
 		
 		/** Entering k and printing the kth least element */
 		System.out.println("Enter a number between 1 and " + size + ".");
@@ -114,7 +142,9 @@ public class Quick_select {
 		quickSelect(array, 0, size-1, k);
 		
 		/** Part B */
-		quickSelectMax(array, 0, size - 1);
+		System.out.println("How many max numbers?");
+		int m = scan.nextInt();
+		quickSelectMax(array, 0, size - 1, m);
 		
 		scan.close();
 	}
